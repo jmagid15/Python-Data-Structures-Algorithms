565416fe15ea023b4f615dd9ee91abdbb452f894
Jake Magid <jake.magid@gmail.com>
2018-04-02 17:13:50 -0400

    Implement queue with list

--- queue.py
+++ queue.py
@@ -1,28 +1,15 @@
-from listnode import ListNode
-
 class Queue():
     def __init__(self):
-        self.first = None
-        self.last = None
+        self.data = []
 
-    def isEmpty(self):
-        return self.first is None
-
-    def enqueue(self, data):
-        node = ListNode(data)
-        if self.last:
-            self.last.next = node
-        self.last = node
-
-        if self.first is None:
-            self.first = self.last
+    def enqueue(self, val):
+        self.data.append(val)
 
     def dequeue(self):
-        assert not self.isEmpty(), 'empty queue'
-        data = self.first.data
-        self.first = self.first.next
-        return data
+        return self.data.pop(0)
 
     def peek(self):
-        assert not self.isEmpty(), 'empty queue'
-        return self.first.data
+        return self.data[0]
+
+    def isEmpty(self):
+        return len(self.data) == 0
