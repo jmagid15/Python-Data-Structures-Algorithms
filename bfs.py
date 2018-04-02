db4b9fea5a8f0e4a760688d7acbc1c7d216c8ee5
Jake Magid <jake.magid@gmail.com>
2018-04-02 19:40:00 -0400

    BFS on adjacency lists

--- bfs.py
+++ bfs.py
@@ -7,6 +7,12 @@ class TreeNode():
         self.right = None
 
 
+class GraphNode():
+    def __init__(self,data):
+        self.data = data
+        self.neighbors = []
+
+
 def bf_traversal(root):
     q = Queue()
     q.enqueue(root)
@@ -38,6 +44,43 @@ def bf_traversal_yield(root):
         if curr.right:
             q.enqueue(curr.right)
 
+
+def bf_graph(root):
+    q = Queue()
+    seen_it = set()
+    path = []
+
+    q.enqueue(root)
+    seen_it.add(root)
+
+    while not q.isEmpty():
+        curr = q.dequeue()
+        path.append(curr.data)
+
+        for neighbor in curr.neighbors:
+            if neighbor not in seen_it:
+                q.enqueue(neighbor)
+                seen_it.add(neighbor)
+    return path
+
+def bf_graph_adjacency(start_node_idx, adjacency_list):
+    q = Queue()
+    seen_it = set()
+    path = []
+
+    q.enqueue(start_node_idx)
+    seen_it.add(start_node_idx)
+
+    while not q.isEmpty():
+        curr_node = q.dequeue()
+        path.append(curr_node)
+
+        for node in adjacency_list[curr_node]:
+            if node not in seen_it:
+                q.enqueue(node)
+                seen_it.add(node)
+
+    return path
 
 # root = SomeNode()
 #
